c2d9281eca1647bcc94d908bdfea20e8aeb2ee1c
Robin Bal <balrobin@hotmail.com>
2024-09-24 11:25:22 +0200

    init

---
+++ Application/src/App.jsx
@@ -0,0 +1,15 @@
+import Coffee from "./components/Coffee";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Coffee />
+    </>
+  );
+}
+
+export default App;
