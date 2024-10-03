be4be63aa260c0a71941dd49a478e63bebf8c58a
MohamedElmi10 <144214489+MohamedElmi10@users.noreply.github.com>
2024-10-03 10:44:41 +0200

    slight changes

---
+++ src/Page.jsx
@@ -0,0 +1,16 @@
+import { Header, Coffee, NavLinks, Tea, AboutUs, Footer } from "javajunction-frontend"
+
+function App() {
+  return (
+    <>
+      <NavLinks />
+      <Header />
+      <Coffee />
+      <Tea />
+      <AboutUs />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
