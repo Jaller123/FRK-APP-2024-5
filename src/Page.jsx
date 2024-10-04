8fde7c7a382977c5a122d393210c333ffe5cb183
MohamedElmi10 <144214489+MohamedElmi10@users.noreply.github.com>
2024-10-04 12:50:08 +0200

    changes to app.jsx

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,14 +1,11 @@
-<<<<<<< HEAD
+
 import { Header, Coffee, Tea, AboutUs, Footer, NavLinks, AddFooter } from "javajunction-frontend"
-=======
-import { Header, Coffee, NavLinks, Tea, AboutUs, Footer, Hero } from "javajunction-frontend"
->>>>>>> 55cf2b64be767ad466c012ec057252a1f154f75a
 
 function App() {
   return (
     <>
       <NavLinks />
-      <Hero />
+
       <Header />
       <Coffee />
       <Tea />
