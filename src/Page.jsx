285d37929352fe507f060e1068651ab176081985
MohamedElmi10 <144214489+MohamedElmi10@users.noreply.github.com>
2024-10-09 10:13:32 +0200

    tweaks

--- src/Page.jsx
+++ src/Page.jsx
@@ -1,27 +1,63 @@
-
-import { Header, Coffee, Tea, AboutUs, Footer, NavLinks, AddFooter, Hero } from "javajunction-frontend"
-import "./index.css"
+import { Header, Coffee, Tea, AboutUs, Footer, AddFooter, Hero, Orders } from "javajunction-frontend";
+import "./index.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, { useRef } from "react";
 
 function App() {
+  // Create refs for each section
+  const coffeeRef = useRef(null);
+  const teaRef = useRef(null);
+  const aboutUsRef = useRef(null);
+
+  // Scroll to the appropriate section
+  const scrollToSection = (sectionRef) => {
+    if (sectionRef.current) {
+      sectionRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <>
-
-
       <Router>
-        <Header customNavbarStyle="customNavbarStyle" customNavLinkClass="customNavLinkClass" navLinksClassName="NavLinksclassName" />
         <Routes>
-          {/* Default route (home) can be Coffee */}
-          <Route path="/" element={<Hero />} />
 
-          {/* Nested routes for Tea and AboutUs */}
-          <Route path="/coffee" element={<Coffee />} />
-          <Route path="/tea" element={<Tea />} />
-          <Route path="/about-us" element={<AboutUs />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <Header
+                  customNavbarStyle="customNavbarStyle"
+                  customNavLinkClass="customNavLinkClass"
+                  navLinksClassName="NavLinksclassName"
+                  scrollToSection={scrollToSection}
+                  coffeeRef={coffeeRef}
+                  teaRef={teaRef}
+                  aboutUsRef={aboutUsRef}
+                />
+                <Hero />
 
+
+                <div ref={coffeeRef}>
+                  <Coffee />
+                </div>
+
+                <div ref={teaRef}>
+                  <Tea />
+                </div>
+
+                <div ref={aboutUsRef}>
+                  <AboutUs />
+                </div>
+
+                <Footer />
+                <AddFooter />
+              </>
+            }
+          />
+
+
+          <Route path="/orders" element={<Orders />} />
         </Routes>
-        <Footer />
-        <AddFooter />
       </Router>
     </>
   );
